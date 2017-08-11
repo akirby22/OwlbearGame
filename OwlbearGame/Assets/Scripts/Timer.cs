8998394d798bc03e3d4092c231a9d356c6844558
Aaron Kirby <Aaron_Kirby22@yahoo.com>
2017-08-11 18:45:03 -0500

    Sec 5.33-5.34 Canvas and Timer
    5.33- Created Canvas
    5.34- Creater Timer prefab and script. Timer seems to be behind the
    trees. Not sure  if it's a layering issue or if it will be solved in the
    tutorial

---
+++ OwlbearGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+
+	public float valueRemaining = 10;
+	public float valueMaximum = 30;
+	public float valueConsumptionRate = 1f;
+
+	public Text timerText;
+
+	public bool gameInProgress = true;
+
+	void Update () {
+		if (gameInProgress) {
+			valueRemaining -= Time.deltaTime * valueConsumptionRate;
+			timerText.text = "TIme Remaining: " + Mathf.Round(valueRemaining).ToString ();
+
+			if (valueRemaining < 0) {
+				gameInProgress = false;
+				endGameWithFailure ();
+			}
+		}
+	}
+
+	void endGameWithFailure() {
+		
+	}
+}
